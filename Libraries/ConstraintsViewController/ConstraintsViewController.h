723b3ab35d7276fab5347189a8bd395e56f8ba62
neogeek <neogeek0101@gmail.com>
2013-10-10 16:28:03 -0400

    Added new method to the ContstraintsViewController which allows you to specify the view objects are being added to.

--- Libraries/ConstraintsViewController/ConstraintsViewController.h
+++ Libraries/ConstraintsViewController/ConstraintsViewController.h
@@ -10,6 +10,8 @@
 
 @interface ConstraintsViewController : UIViewController
 
+- (void)setConstraintsForViews:(NSDictionary *)views inView:(UIView *)view visualFormats:(NSArray *)visualFormats metrics:(NSDictionary *)metrics options:(NSLayoutFormatOptions)options;
+
 - (void)setConstraintsForViews:(NSDictionary *)views visualFormats:(NSArray *)visualFormats metrics:(NSDictionary *)metrics options:(NSLayoutFormatOptions)options;
 
 @end
--- Libraries/ConstraintsViewController/ConstraintsViewController.m
+++ Libraries/ConstraintsViewController/ConstraintsViewController.m
@@ -27,9 +27,10 @@
 /**
  * Streamlines the creation of Auto Layout Constraints using VFL (Visual Format Language)
  *
- *  [self setConstraintsForViews:NSDictionaryOfVariableBindings(button) visualFormats:@[@"|-[button]-|", @"V:[button(height)]-|"] metrics:@{@"height":@50.0} options:0];
+ *  [self setConstraintsForViews:NSDictionaryOfVariableBindings(button) inView:self visualFormats:@[@"|-[button]-|", @"V:[button(height)]-|"] metrics:@{@"height":@50.0} options:0];
  *
  * @param {NSDictionary} setConstraintsForViews A NSDictionaryOfVariableBindings object containing the views to be positioned through autolayout.
+ * @param {UIView} inView A view controller.
  * @param {NSArray} visualFormats An NSArray of visual format strings.
  * @param {NSDictionary} metrics An NSDictionary object containing visual format variables.
  * @param {NSLayoutFormatOptions} options An NSLayoutFormatOptions object containing one (or two) alignment options.
@@ -37,14 +38,14 @@
  * @api public
  */
 
-- (void)setConstraintsForViews:(NSDictionary *)views visualFormats:(NSArray *)visualFormats metrics:(NSDictionary *)metrics options:(NSLayoutFormatOptions)options;
+- (void)setConstraintsForViews:(NSDictionary *)views inView:(UIView *)parentView visualFormats:(NSArray *)visualFormats metrics:(NSDictionary *)metrics options:(NSLayoutFormatOptions)options;
 {
 
     for (UIView *view in [views allValues]) {
 
-        if (![view isDescendantOfView:self.view]) {
+        if (![view isDescendantOfView:parentView]) {
 
-            [self.view addSubview:view];
+            [parentView addSubview:view];
 
         }
 
@@ -60,10 +61,32 @@
 
     for (NSString *format in visualFormats) {
 
-        [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:format options:options metrics:allMetrics views:views]];
+        [parentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:format options:options metrics:allMetrics views:views]];
 
     }
 
 }
+
+
+/**
+ * Streamlines the creation of Auto Layout Constraints using VFL (Visual Format Language)
+ *
+ *  [self setConstraintsForViews:NSDictionaryOfVariableBindings(button) visualFormats:@[@"|-[button]-|", @"V:[button(height)]-|"] metrics:@{@"height":@50.0} options:0];
+ *
+ * @param {NSDictionary} setConstraintsForViews A NSDictionaryOfVariableBindings object containing the views to be positioned through autolayout.
+ * @param {NSArray} visualFormats An NSArray of visual format strings.
+ * @param {NSDictionary} metrics An NSDictionary object containing visual format variables.
+ * @param {NSLayoutFormatOptions} options An NSLayoutFormatOptions object containing one (or two) alignment options.
+ * @return {void}
+ * @api public
+ */
+
+- (void)setConstraintsForViews:(NSDictionary *)views visualFormats:(NSArray *)visualFormats metrics:(NSDictionary *)metrics options:(NSLayoutFormatOptions)options;
+{
+
+    [self setConstraintsForViews:views inView:self.view visualFormats:visualFormats metrics:metrics options:options];
+
+}
+
 
 @end
